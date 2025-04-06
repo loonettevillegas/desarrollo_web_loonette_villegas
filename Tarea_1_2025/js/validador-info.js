8b3177b39f54c65d2e391726725cb863221c9ca9
loonettevillegas <loonette.villegas@ug.uchile.cl>
2025-04-05 22:44:06 -0300

    Adding

---
+++ Tarea_1_2025/js/validador-info.js
@@ -0,0 +1,679 @@
+
+
+
+
+let regiones = {
+    "Arica y Parinacota":[
+        "Arica",
+        "Camarones",
+        "General Lagos",
+        "Putre"
+    ],
+    
+    "Tarapacá":[
+        "Alto Hospicio",
+        "Camiña",
+        "Colchane",
+        "Huara",
+        "Iquique",
+        "Pica",
+        "Pozo Almonte"
+    ],
+
+    "Antofogasta":[
+        "Antofagasta",
+        "Calama",
+        "María Elena",
+        "Mejillones",
+        "Ollagüe",
+        "San Pedro de Atacama",
+        "Sierra Gorda",
+        "Taltal",
+        "Tocopilla"
+    ],
+
+    "Atacama":[
+        "Alto del Carmen",
+        "Caldera",
+        "Chañaral",
+        "Copiapó",
+        "Diego de Almagro",
+        "Freirina",
+        "Huasco",
+        "Tierra Amarilla",
+        "Vallenar"
+
+    ],
+
+    "Coquimbo":[
+        "Andacollo",
+        "Canela",
+        "Combarbalá",
+        "Coquimbo",
+        "Illapel",
+        "La Higuera",
+        "La Serena",
+        "Los Vilos",
+        "Monte Patria",
+        "Ovalle",
+        "Paihuano",
+        "Punitaqui",
+        "Río Hurtado",
+        "Salamanca",
+        "Vicuña"
+    ],
+
+    "Valparaíso":[
+        "Algarrobo",
+        "Cabildo",
+        "Calera",
+        "Calle Larga",
+        "Cartagena",
+        "Casablanca",
+        "Catemu",
+        "Concón",
+        "El Quisco",
+        "El Tabo",
+        "Hijuelas",
+        "Isla de Pascua",
+        "Juan Fernández",
+        "La Cruz",
+        "La Ligua",
+        "Limache",
+        "Llaillay",
+        "Los Andes",
+        "Nogales",
+        "Olmué",
+        "Panquehue",
+        "Papudo",
+        "Petorca",
+        "Puchuncaví",
+        "Putaendo",
+        "Quillota",
+        "Quilpué",
+        "Quintero",
+        "Rinconada",
+        "San Antonio",
+        "San Esteban",
+        "San Felipe",
+        "Santa María",
+        "Santo Domingo",
+        "Valparaíso",
+        "Villa Alemana",
+        "Viña del Mar",
+        "Zapallar",
+
+    ],
+
+    "Lib. Gral. Bernardo O'Higgins":[
+        "Chépica",
+        "Chimbarongo",
+        "Codegua",
+        "Coínco",
+        "Coltauco",
+        "Doñihue",
+        "Graneros",
+        "La Estrella",
+        "Las Cabras",
+        "Litueche",
+        "Lolol",
+        "Machalí",
+        "Malloa",
+        "Marchihue",
+        "Mostazal",
+        "Nancagua",
+        "Navidad",
+        "Olivar",
+        "Palmilla",
+        "Paredones",
+        "Peralillo",
+        "Peumo",
+        "Pichidegua",
+        "Pichilemu",
+        "Placilla",
+        "Pumanque",
+        "Quinta de Tilcoco",
+        "Rancagua",
+        "Rengo",
+        "Requínoa",
+        "San Fernando",
+        "Santa Cruz",
+        "San Vicente"
+    ],
+
+    "Maule":[
+        "Cauquenes",
+        "Chanco",
+        "Colbún",
+        "Constitución",
+        "Curepto",
+        "Curicó",
+        "Empedrado",
+        "Hualañé",
+        "Licantén",
+        "Linares",
+        "Longaví",
+        "Maule",
+        "Molina",
+        "Parral",
+        "Pelarco",
+        "Pelluhue",
+        "Pencahue",
+        "Rauco",
+        "Retiro",
+        "Río Claro",
+        "Romeral",
+        "Sagrada Familia",
+        "San Clemente",
+        "San Javier",
+        "San Rafael",
+        "Talca",
+        "Teno",
+        "Vichuquén",
+        "Villa Alegre",
+        "Yerbas Buenas"
+    ],
+
+    "Ñuble":[
+        "Bulnes",
+        "Chillán",
+        "Chillán Viejo",
+        "Cobquecura",
+        "Coelemu",
+        "Coihueco",
+        "El Carmen",
+        "Ninhue",
+        "Ñiquén",
+        "Pemuco",
+        "Pinto",
+        "Portezuelo",
+        "Quillón",
+        "Quirihue",
+        "Ránquil",
+        "San Carlos",
+        "San Fabián",
+        "San Ignacio",
+        "San Nicolás",
+        "Treguaco",
+        "Yungay"
+    ],
+    
+   "Biobío":[
+        "Alto Biobío",
+        "Antuco",
+        "Arauco",
+        "Cabrero",
+        "Cañete",
+        "Chiguayante",
+        "Concepción",
+        "Contulmo",
+        "Coronel",
+        "Curanilahue",
+        "Florida",
+        "Hualpén",
+        "Hualqui",
+        "Laja",
+        "Lebu",
+        "Los Alamos",
+        "Los Angeles",
+        "Lota",
+        "Mulchén",
+        "Nacimiento",
+        "Negrete",
+        "Penco",
+        "Quilaco",
+        "Quilleco",
+        "San Pedro de la Paz",
+        "San Rosendo",
+        "Santa Bárbara",
+        "Santa Juana",
+        "Talcahuano",
+        "Tirúa",
+        "Tomé",
+        "Tucapel",
+        "Yumbel"
+    ],
+    
+    "La Araucanía":[
+        "Angol",
+        "Carahue",
+        "Cholchol",
+        "Collipulli",
+        "Cunco",
+        "Curacautín",
+        "Curarrehue",
+        "Ercilla",
+        "Freire",
+        "Galvarino",
+        "Gorbea",
+        "Lautaro",
+        "Loncoche",
+        "Lonquimay",
+        "Los Sauces",
+        "Lumaco",
+        "Melipeuco",
+        "Nueva Imperial",
+        "Padre Las Casas",
+        "Perquenco",
+        "Pitrufquén",
+        "Pucón",
+        "Purén",
+        "Renaico",
+        "Saavedra",
+        "Temuco",
+        "Teodoro Schmidt",
+        "Toltén",
+        "Traiguén",
+        "Victoria",
+        "Vilcún",
+        "Villarrica"
+    ],
+    
+    "Los Ríos":[
+        "Corral",
+        "Futrono",
+        "Lago Ranco",
+        "Lanco",
+        "La Unión",
+        "Los Lagos",
+        "Máfil",
+        "Mariquina",
+        "Paillaco",
+        "Panguipulli",
+        "Río Bueno",
+        "Valdivia"
+    ],
+    
+    "Los Lagos":[
+        "Ancud",
+        "Calbuco",
+        "Castro",
+        "Chaitén",
+        "Chonchi",
+        "Cochamó",
+        "Curaco de Vélez",
+        "Dalcahue",
+        "Fresia",
+        "Frutillar",
+        "Futaleufú",
+        "Hualaihué",
+        "Llanquihue",
+        "Los Muermos",
+        "Maullín",
+        "Osorno",
+        "Palena",
+        "Puerto Montt",
+        "Puerto Octay",
+        "Puerto Varas",
+        "Puqueldón",
+        "Purranque",
+        "Puyehue",
+        "Queilén",
+        "Quellón",
+        "Quemchi",
+        "Quinchao",
+        "Río Negro",
+        "San Juan de la Costa",
+        "San Pablo"
+    ],
+    
+    "Aysén del General Carlos Ibáñez del Campo":[
+        "Aysén",
+        "Chile Chico",
+        "Cisnes",
+        "Cochrane",
+        "Coyhaique",
+        "Guaitecas",
+        "Lago Verde",
+        "O'Higgins",
+        "Río Ibáñez",
+        "Tortel"
+    ],
+    
+   "Magallanes y Antártica Chilena":[
+        "Antártica",
+        "Cabo de Hornos",
+        "Laguna Blanca",
+        "Natales",
+        "Porvenir",
+        "Primavera",
+        "Punta Arenas",
+        "Río Verde",
+        "San Gregorio",
+        "Timaukel",
+        "Torres del Paine"
+    ],
+    
+   "Metropolitana de Santiago":[
+        "Alhué",
+        "Buin",
+        "Calera de Tango",
+        "Cerrillos",
+        "Cerro Navia",
+        "Colina",
+        "Conchalí",
+        "Curacaví",
+        "El Bosque",
+        "El Monte",
+        "Estación Central",
+        "Huechuraba",
+        "Independencia",
+        "Isla de Maipo",
+        "La Cisterna",
+        "La Florida",
+        "La Granja",
+        "Lampa",
+        "La Pintana",
+        "La Reina",
+        "Las Condes",
+        "Lo Barnechea",
+        "Lo Espejo",
+        "Lo Prado",
+        "Macul",
+        "Maipú",
+        "María Pinto",
+        "Melipilla",
+        "Ñuñoa",
+        "Padre Hurtado",
+        "Paine",
+        "Pedro Aguirre Cerda",
+        "Peñaflor",
+        "Peñalolén",
+        "Pirque",
+        "Providencia",
+        "Pudahuel",
+        "Puente Alto",
+        "Quilicura",
+        "Quinta Normal",
+        "Recoleta",
+        "Renca",
+        "San Bernardo",
+        "San Joaquín",
+        "San José de Maipo",
+        "San Miguel",
+        "San Pedro",
+        "San Ramón",
+        "Santiago",
+        "Talagante",
+        "Tiltil",
+        "Vitacura"
+    ],
+          
+}
+
+
+const choose_region= document.getElementById('region'),
+        choose_comuna=document.getElementById('comuna')
+
+        choose_comuna.disabled=true
+        
+for(let region  in regiones ){
+choose_region.options[choose_region.options.length] = new Option(region, region)
+
+}
+choose_region.onchange= (n) =>{
+choose_comuna.disabled= false
+choose_comuna.length=1
+
+let comuna = regiones[n.target.value]
+
+        for(let i=0; i<comuna.length; i++){
+            choose_comuna.options[choose_comuna.options.length] = new Option(comuna[i], comuna[i])
+        }
+
+    }
+
+
+
+ 
+
+//hidden value for theme description
+ document.getElementById("theme_select_form").addEventListener("change", function() {
+            let inputContainer = document.getElementById("Other-selection");
+            if (this.value=="10") {
+                inputContainer.style.display = "block"; 
+            } else {
+                inputContainer.style.display = "none";
+            }
+        });
+
+
+
+
+
+/*hidden value for channel description*/
+ document.getElementById("contact_select_form").addEventListener("change", function() {
+            let inputContainer = document.getElementById("contact-selection");
+            if (this.value) {
+                inputContainer.style.display = "block"; 
+            } else {
+                inputContainer.style.display = "none";
+            }
+        });
+
+const validateAnyText = (text,x,y) => {
+  if(!text) return false;
+  let lengthValid = text.trim().length >= x && text.trim().length <= y;
+  
+  return lengthValid;
+}       
+
+//funciones del aux
+const validateName = (name) => {
+  if(!name) return false;
+  let lengthValid = name.trim().length >= 4 && name.trim().length <= 200;
+  
+  return lengthValid;
+}
+
+const validateEmail = (email) => {
+  if (!email) return false;
+  let lengthValid = email.length < 100 ;
+
+  let re = /^[\w.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  let formatValid = re.test(email);
+  return lengthValid && formatValid;
+};
+
+const validatePhoneNumber = (phoneNumber) => {
+  if (!phoneNumber) return false;
+  let lengthValid = phoneNumber.length >= 8;
+
+  let re = /^[0-9]+$/;
+  let formatValid = re.test(phoneNumber);
+
+  return lengthValid && formatValid;
+};
+
+const validateFiles = (files) => {
+  if (!files) return false;
+  let lengthValid = 1 <= files.length && files.length <= 5;
+  let typeValid = true;
+  for (const file of files) {
+    let fileFamily = file.type.split("/")[0];
+    typeValid &&= fileFamily == "image" || file.type == "application/pdf";
+  }
+  return lengthValid && typeValid;
+};
+
+const validateSelect = (select) => {
+  if(!select) return false;
+  return true
+}
+const validate_regions = (index)=>{
+    
+    return index!=0 ;
+}
+
+const validate_comuna = (index)=>{
+    return index>0;
+}
+
+const validateInitialDate = (date)=>{
+ if (!date) return false;
+ let re = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
+ let formatValid = re.test(date);
+
+return  formatValid;
+ }
+const validateFinalDate = (date1, date2) =>{
+ if (!date2) return false;
+ let re = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
+ let formatValid = re.test(date2);
+const d1 = new Date(date1)
+const d2 = new Date(date2)
+let validDate = d1<d2 
+return  formatValid && validDate;
+ }
+
+
+
+const validateForm = ()=> {
+
+
+//extrayendo info
+    let myForm = document.forms["first-form"];
+    let email = myForm["email"].value;
+    let phoneNumber = myForm["contacto"].value;
+    let name = myForm["name_input"].value;
+    let region_select= myForm["region"].selectedIndex;
+    let comuna_select=myForm["comuna"].selectedIndex;
+    let files = myForm["photo_1"].files;
+    let theme = myForm["theme_select_form"].value;
+    let time = myForm["date-initial-form"].value;
+    let time2 = myForm["date-end-form"].value;
+    let description_sector = myForm["sector_text"].value;
+    let description_contact = myForm["description-contact-input"].value;
+    let contact = myForm["contact_select_form"].value
+   
+
+
+
+//listando invalida inputs
+    let invalidInputs = [];
+    let isValid = true;
+    const setInvalidInput = (inputName) => {
+        invalidInputs.push(inputName);
+        isValid &&= false;
+    };
+
+//revisando qué es inválido
+    if(!validate_regions(region_select)){
+            
+            setInvalidInput("Región");
+    }
+        
+    if(!validate_comuna(comuna_select)){
+        setInvalidInput("Comuna");
+            
+    }
+
+    if (!validateAnyText(description_sector,0,100)) {
+        setInvalidInput("Descripción del sector");
+        }
+    if (!validateName(name)) {
+            setInvalidInput("Nombre organizador");
+    }
+    
+    if (!validateEmail(email)) {
+            setInvalidInput("Email");
+    }
+    
+    if (!validatePhoneNumber(phoneNumber)) {
+            setInvalidInput("Número de contacto");
+
+    }
+    if(!validateSelect(contact)){
+        setInvalidInput("Selección contacto")
+    }
+    if (validateSelect(contact)&&!validateAnyText(description_contact,4,50)){
+        setInvalidInput("Descripción del contacto");
+        }
+        
+     if(!validateInitialDate(time)){
+        setInvalidInput("Fecha y tiempo de inicio");
+            
+        }
+    if(!validateFinalDate(time,time2)){
+        setInvalidInput("Fecha y tiempo de fin");
+            
+        }
+    
+    if (!validateSelect(theme)) {
+        setInvalidInput("Tema");
+        }
+    
+    if (!validateFiles(files)) {
+            setInvalidInput("Fotos");
+    }
+            
+    
+   
+    
+
+
+
+  let validationBox = document.getElementById("val-box");
+  let validationMessageElem = document.getElementById("val-msg");
+  let validationListElem = document.getElementById("val-list");
+        
+    if (!isValid) {
+            validationListElem.textContent = "";
+            // agregar elementos inválidos al elemento val-list.
+            for (input of invalidInputs) {
+            let listElement = document.createElement("li");
+            listElement.innerText = input;
+            validationListElem.append(listElement);
+            }
+            // establecer val-msg
+            validationMessageElem.innerText = "Los siguientes campos son inválidos:";
+
+            // aplicar estilos de error
+            validationBox.style.backgroundColor = "#ffdddd";
+            validationBox.style.borderLeftColor = "#f44336";
+
+            // hacer visible el mensaje de validación
+            validationBox.hidden = false;
+        } else {
+          
+            // Ocultar el formulario
+            myForm.style.display = "none";
+            
+
+            // establecer mensaje de éxito
+            validationMessageElem.innerText = "¿Está seguro que desea agregar esta actividad?";
+            validationListElem.textContent = "";
+            
+            // aplicar estilos de éxito
+            validationBox.style.backgroundColor = "#ddffdd";
+            validationBox.style.borderLeftColor = "#4CAF50";
+
+            // Agregar botones para enviar el formulario o volver
+            let submitButton = document.createElement("button");
+            submitButton.innerText = "Si, estoy seguro";
+            submitButton.style.marginRight = "10px";
+            submitButton.addEventListener("click", () => {
+             validationMessageElem.innerText = "Hemos recibido su información, muchas gracias y suerte en su actividad";
+             submitButton.style.display = "none"
+             backButton.style.display = "none"
+             
+
+            });
+
+            let backButton = document.createElement("button");
+            backButton.innerText = "No, no estoy seguro, quiero volver.";
+            backButton.addEventListener("click", () => {
+            
+            myForm.style.display = "block";
+            validationBox.hidden = true;
+            });
+
+            validationListElem.appendChild(submitButton);
+            validationListElem.appendChild(backButton);
+
+            // hacer visible el mensaje de validación
+            validationBox.hidden = false;
+        }
+        };
+let submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", validateForm);
